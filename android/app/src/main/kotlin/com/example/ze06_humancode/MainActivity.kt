a3d39d2935920b558d900204e5eba3c02e263ba2
Natthapol14 <tyuioopp707@hotmail.com>
2024-02-08 22:41:47 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/ze06_humancode/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ze06_humancode
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/questions.dart
@@ -0,0 +1,106 @@
+import 'question.dart';
+
+const List<Question> questions = [
+  Question(
+    question: '1. Genshin Impact เป็นเกมแนวอะไร?',
+    correctAnswerIndex: 0,
+    options: [
+      'ก.  Open world, Rpg',
+      'ข.  Mmorpg',
+      'ค.  Turn-Based',
+      'ง.  FPS',
+    ],
+  ),
+  Question(
+    question: '2. Genshin Impact เมืองแรกที่ผู้เล่นได้เล่น ชื่อเมืองอะไร?',
+    correctAnswerIndex: 2,
+    options: [
+      'ก. Sumeru',
+      'ข. Inazuma',
+      'ค. Mondstadt',
+      'ง. Liyue',
+    ],
+  ),
+  Question(
+    question: '3. Genshin Impact ตัวละครแรกที่ผู้เล่นเจอในการเล่นคือใคร?',
+    correctAnswerIndex: 3,
+    options: [
+      'ก.  Kaeya',
+      'ข.  Diluc',
+      'ค.  Amber',
+      'ง.  Paimon',
+    ],
+  ),
+  Question(
+    question: '4. Genshin Impact ณ ปัจจุบัน V.4.4 มีเทพทั้งหมดกี่องค์?',
+    correctAnswerIndex: 3,
+    options: [
+      'ก.  1 องค์',
+      'ข.  2 องค์',
+      'ค.  3 องค์',
+      'ง.  4 องค์',
+    ],
+  ),
+  Question(
+    question: '5. Genshin impact เมือง Mondstadt ใครเป็นเทพประจำเมือง?',
+    correctAnswerIndex: 2,
+    options: [
+      'ก. Nahida',
+      'ข. Raiden Shogun',
+      'ค. Venti',
+      'ง. Zhongli',
+    ],
+  ),
+  Question(
+    question: '6. Genshin impact เมือง Liyue ใครเป็นเทพประจำเมืองเมือง?',
+    correctAnswerIndex: 1,
+    options: [
+      'ก. Xianyun',
+      'ข. Zhongli',
+      'ค. Ayaka',
+      'ง. Ningguang',
+    ],
+  ),
+  Question(
+    question: '7. Genshin impact เมือง Inazuma ใครเป็นเทพประจำเมืองเมือง?',
+    correctAnswerIndex: 1,
+    options: [
+      'ก. Ayaka',
+      'ข. Raiden Shogun',
+      'ค. Yae miko',
+      'ง. Sara',
+    ],
+  ),
+  Question(
+    question: '8. Genshin impact เมือง Sumeru ใครเป็นเทพประจำเมืองเมือง?',
+    correctAnswerIndex: 0,
+    options: [
+      'ก. Nahida',
+      'ข. Alhaitham',
+      'ค. Dehya',
+      'ง. Baizhu',
+    ],
+  ),
+  Question(
+    question:
+        ' 9. Genshin impact ใครเป็นเทพที่เก่งที่สุด ณ ปัจจุบัน อ้างอิงตามเนื้อเรื่อง?',
+    correctAnswerIndex: 2,
+    options: [
+      'ก. Nahida',
+      'ข. Venti',
+      'ค. Zhongli',
+      'ง. Raiden Shogun',
+    ],
+  ),
+  Question(
+    question:
+        ' 10. Genshin impact กาชาตัวละครจะการันตีที่โรลเท่าไร?',
+    correctAnswerIndex: 1,
+    options: [
+      'ก. 80 โรล',
+      'ข. 90 โรล',
+      'ค. 100 โรล',
+      'ง. 99 โรล',
+    ],
+  ),
+];
